c9281e20bfc264a70440c3b190a477984a70a7a3
GIPYEONG CHUNG <gipyeongchung@GIPYEONGui-MacBookPro.local>
2022-09-14 13:40:31 +0900

    K 번째 수

--- PYEONG/backjoon/src/_1300_K번째수_.java
+++ PYEONG/backjoon/src/_1300_K번째수_.java
@@ -8,11 +8,45 @@ public class _1300_K번째수_ {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int K = Integer.parseInt(br.readLine());
-        int[] B = new int[N * N];
-        for (int n = 0; n < N * N; n++) {
-            B[n] = (((n / N) + 1) * ((n % N) + 1));
+        // int[] B = new int[N * N];
+        // for (int n = 0; n < N * N; n++) {
+        //     B[n] = (((n / N) + 1) * ((n % N) + 1));
+        // }
+        // Arrays.sort(B);
+        // System.out.println(B[K]);
+        
+        /* 
+        // 틀림
+        long left = 1, right = N * N, answer = 0;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long cnt = 0;
+            for (int i = 1; i <= N; i++) {
+                cnt += Math.min(mid / i, N);
+            }
+            if (cnt < K) {
+                left = mid + 1;
+            } else {
+                answer = mid;
+                right = mid - 1;
+            }
         }
-        Arrays.sort(B);
-        System.out.println(B[K]);
+        System.out.println(answer);
+        */
+        long left = 1, right = K, answer = 0;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long cnt = 0;
+            for (int i = 1; i <= N; i++) {
+                cnt += Math.min(mid / i, N);
+            }
+            if (cnt < K) {
+                left = mid + 1;
+            } else {
+                answer = mid;
+                right = mid - 1;
+            }
+        }
+        System.out.println(answer);
     }
 }
